402e0bf6d6a4ffe22bb5f2b8bc03825024d87ca9
damoye <damoye1993@hotmail.com>
2025-12-09 02:03:13 +0800

    add omarchy.sh

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 ```bash
-# install packages
-install.sh
-# stow dotfiles
 cd ~
 git clone git@github.com:damoye/dotfiles.git
 cd dotfiles
---
+++ scripts/omarchy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+sudo pacman -S\
+  fd\
+  fzf\
+  ghostty\
+  go\
+  opencode\
+  stow
+
+yay -S\
+  helium-browser-bin\
+  clash-verge-rev-bin\
+  yesplaymusic
